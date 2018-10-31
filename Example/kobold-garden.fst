e5517f955b3b42c6aa1409296eaccf64150f0ef1
Martin Allerton <martin@3sixtysystems.com>
2018-10-31 14:57:13 +0000

    initial upload

---
+++ Example/kobold-garden.fst
@@ -0,0 +1,107 @@
+name = kobold-garden
+type = body+head
+scale = 1
+filename = kobold-garden/kobold-garden.fbx
+texdir = kobold-garden/textures
+script = kobold-garden/scripts/flowapp.js
+script = kobold-garden/scripts/materialSwap.js
+joint = jointLean = Spine
+joint = jointEyeLeft = LeftEye
+joint = jointLeftHand = LeftHand
+joint = jointNeck = Neck
+joint = jointHead = Head
+joint = jointEyeRight = RightEye
+joint = jointRightHand = RightHand
+joint = jointRoot = Hips
+freeJoint = LeftArm
+freeJoint = LeftForeArm
+freeJoint = RightArm
+freeJoint = RightForeArm
+jointIndex = Armature = 6
+jointIndex = RightHandMiddle2 = 36
+jointIndex = Lamp.003 = 2
+jointIndex = RightLeg = 13
+jointIndex = Jaw2 = 76
+jointIndex = LeftShoulder = 39
+jointIndex = RightUpLeg = 12
+jointIndex = LeftHandIndex1 = 47
+jointIndex = Hips = 7
+jointIndex = RightHandMiddle3 = 37
+jointIndex = simTip1 = 68
+jointIndex = RightHandIndex3 = 29
+jointIndex = simTail1 = 80
+jointIndex = RightHandPinky4 = 26
+jointIndex = RightHandMiddle4 = 38
+jointIndex = RightHandIndex4 = 30
+jointIndex = Jaw_end = 79
+jointIndex = LeftHandThumb4 = 54
+jointIndex = simTip4 = 74
+jointIndex = simTip3.001 = 65
+jointIndex = LeftHandPinky3 = 45
+jointIndex = LeftHandPinky1 = 43
+jointIndex = LeftHandIndex2 = 48
+jointIndex = simTail5 = 84
+jointIndex = LeftToeBase = 11
+jointIndex = RightHandPinky1 = 23
+jointIndex = simTip6 = 70
+jointIndex = RightHandIndex1 = 27
+jointIndex = LeftHandIndex3 = 49
+jointIndex = LeftHandMiddle3 = 57
+jointIndex = LeftHandThumb1 = 51
+jointIndex = simTail3 = 82
+jointIndex = Lamp.001 = 4
+jointIndex = LeftArm = 40
+jointIndex = simTail2 = 81
+jointIndex = RightHandThumb2 = 32
+jointIndex = Body = 5
+jointIndex = RightFoot = 14
+jointIndex = Lamp.004 = 87
+jointIndex = LeftHandMiddle4 = 58
+jointIndex = LeftHand = 42
+jointIndex = simTip6.001 = 71
+jointIndex = simTip5 = 72
+jointIndex = RightHandThumb3 = 33
+jointIndex = simTip4.001 = 75
+jointIndex = Spine2 = 18
+jointIndex = LeftLeg = 9
+jointIndex = Spine = 16
+jointIndex = HeadTop_End = 61
+jointIndex = RightShoulder = 19
+jointIndex = LeftHandMiddle2 = 56
+jointIndex = LeftHandThumb3 = 53
+jointIndex = LeftHandThumb2 = 52
+jointIndex = LeftUpLeg = 8
+jointIndex = LeftHandIndex4 = 50
+jointIndex = simTip1.001 = 69
+jointIndex = simTip3 = 64
+jointIndex = Jaw = 78
+jointIndex = Lamp.002 = 3
+jointIndex = RightHandIndex2 = 28
+jointIndex = Lamp = 1
+jointIndex = simTip2.001 = 67
+jointIndex = Head = 60
+jointIndex = LeftEye = 63
+jointIndex = LeftHandPinky4 = 46
+jointIndex = RightToeBase = 15
+jointIndex = simTip2 = 66
+jointIndex = Camera = 0
+jointIndex = LeftHandPinky2 = 44
+jointIndex = simTip5.001 = 73
+jointIndex = simTail4 = 83
+jointIndex = LeftHandMiddle1 = 55
+jointIndex = LeftForeArm = 41
+jointIndex = simTail6 = 85
+jointIndex = Neck = 59
+jointIndex = RightHandMiddle1 = 35
+jointIndex = Jaw2_end = 77
+jointIndex = LeftFoot = 10
+jointIndex = RightHandPinky3 = 25
+jointIndex = RightHandThumb1 = 31
+jointIndex = RightArm = 20
+jointIndex = RightEye = 62
+jointIndex = Camera.001 = 86
+jointIndex = RightForeArm = 21
+jointIndex = Spine1 = 17
+jointIndex = RightHandThumb4 = 34
+jointIndex = RightHand = 22
+jointIndex = RightHandPinky2 = 24
---
+++ Example/kobold-garden/scripts/materialSwap.js
@@ -0,0 +1,125 @@
+﻿/**
+ * @file Adds the ability to swap materials on the avatar based on pre configured options.
+ * @author Madders
+ */
+
+var materialSwap = function () {
+    var APP_NAME = "SKINS";
+    var SETTING_STORAGE_NAME = "KoboldGardenMaterialSwap"; // <---- Best to change this to the name of the avatar. used to persist setting between script reloads
+    // Link to your app's HTML file
+    var APP_URL = Script.resolvePath("materialSwap.html");
+    var APP_ICON = Script.resolvePath("terrain.svg");
+
+    var materialEntityName = MyAvatar.displayName + "-Avatar-Material-Entity-";
+    var materialOptions = Script.require(Script.resolvePath("materialOptions.json"));
+
+    // Get a reference to the tablet 
+    var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+
+    // The following lines create a button on the tablet's menu screen
+    var button = tablet.addButton({
+        icon: APP_ICON,
+        text: APP_NAME
+    });
+
+    // When user click the app button, we'll display our app on the tablet screen
+    function onClicked() {
+        tablet.gotoWebScreen(APP_URL + "?date=" + new Date());
+    }
+    button.clicked.connect(onClicked);
+
+    // function to load a material option from configuration if it exists
+    var loadMaterialOption = function (optionId) {
+       // loop through all the material options
+        for (var i = 0; i < materialOptions.length; i++) {
+            // check if material option is the one requested
+            if (materialOptions[i].id === optionId) {
+                // clear any existing material entities
+                clearAllMaterialEntities();
+
+                // create any materials that constitue this material option
+                for (var j = 0; j < materialOptions[i].materials.length; j++) {
+                    createAvatarMaterialEntity(materialOptions[i].materials[j]);
+                }
+
+                Settings.setValue(SETTING_STORAGE_NAME + "/ChosenOption", optionId);
+                return;
+            }
+        }
+
+        // should only get here if the supplied optionId isn't found
+        console.log("Supplied optionId not found in materialOptions. Please check spelling and case.");
+    };
+
+    // function to create an avatar material based on a basic material object
+    var createAvatarMaterialEntity = function (material) {
+        return Entities.addEntity({
+            type: "Material",
+            name: materialEntityName + material.materialName,
+            position: MyAvatar.position,
+            userData: "",
+            materialURL: material.materialURL, // url of the material, or use string constant "materialData" to use inline json matareialData
+            scale: {
+                x: 1,
+                y: 1,
+                z: 1
+            },
+            priority: 1,
+            parentMaterialName: "mat::" + material.materialName, // this name must match that used within the avatar model
+            materialData: JSON.stringify(material.materialData),
+            parentID: MyAvatar.sessionUUID
+        }, true);
+    };
+
+    // function used to clear all material entities currently attached to your avatar that follow the creation pattern of this script
+    var clearAllMaterialEntities = function () {
+        var entities = Entities.findEntities(MyAvatar.position, 10);
+        if (entities.length > 0) {
+            for (var index in entities) {
+                var entity = Entities.getEntityProperties(entities[index]);
+                if (entity.clientOnly &&
+                    entity.type === "Material" &&
+                    entity.parentID === MyAvatar.sessionUUID &&
+                    entity.name.indexOf("Avatar-Material-Entity") !== -1) {
+
+                    Entities.deleteEntity(entity.id);
+                    console.log("Found a potential Avatar Material Entity. Removing it.");
+                }
+            }
+        }
+    };
+
+    var onWebEventReceived = function (event) {
+        // Converts the event to a JavasScript Object
+        if (typeof event === "string") {
+            event = JSON.parse(event);
+        }
+
+        if (event.type === "materialSwap-getSettings") {            
+            tablet.emitScriptEvent(JSON.stringify({
+                type: "materialSwap-settings",
+                settings: materialOptions
+            }));
+        }
+
+        if (event.type === "materialSwap-setMaterialOption") {
+            loadMaterialOption(event.optionId);
+        }
+
+        if (event.type === "materialSwap-clearMaterialOption") {
+            clearAllMaterialEntities();
+        }
+    };
+
+    var savedOptionId = Settings.getValue(SETTING_STORAGE_NAME + "/ChosenOption");
+    if (savedOptionId.length) {
+        loadMaterialOption(savedOptionId);
+    }
+
+    tablet.webEventReceived.connect(onWebEventReceived);
+
+    Script.scriptEnding.connect(function () {
+        tablet.removeButton(button);
+        clearAllMaterialEntities();
+    });
+}();
